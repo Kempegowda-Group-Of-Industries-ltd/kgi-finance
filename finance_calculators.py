c89e7be551656002a5cb0078464d026a9c89c618
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2024-09-14 00:00:43 -0400

    finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -3,15 +3,22 @@ from loan_calculator import loan_calculator
 from investment_calculator import investment_growth
 from budget_tracker import budget_tracker
 
-st.title('Finance Calculators & Simulators')
+def main():
+    st.title('Finance Calculators & Simulators')
 
-# Sidebar to choose calculator type
-app_mode = st.sidebar.selectbox("Choose the Calculator", 
-                                ["Loan Calculator", "Investment Growth", "Budget Tracker"])
+    # Sidebar to choose calculator type
+    app_mode = st.sidebar.selectbox(
+        "Choose the Calculator", 
+        ["Loan Calculator", "Investment Growth", "Budget Tracker"]
+    )
 
-if app_mode == "Loan Calculator":
-    loan_calculator()
-elif app_mode == "Investment Growth":
-    investment_growth()
-elif app_mode == "Budget Tracker":
-    budget_tracker()
+    # Display the selected module
+    if app_mode == "Loan Calculator":
+        loan_calculator()
+    elif app_mode == "Investment Growth":
+        investment_growth()
+    elif app_mode == "Budget Tracker":
+        budget_tracker()
+
+if __name__ == "__main__":
+    main()
